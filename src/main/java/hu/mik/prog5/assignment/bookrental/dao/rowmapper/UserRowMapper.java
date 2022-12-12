b83cef1d77485699c5f226ffcf0922572246785c
Tamás Nichter <nichtert99@gmail.com>
2022-12-13 00:49:51 +0100

    feat(mapping): add user row mapper

---
+++ src/main/java/hu/mik/prog5/assignment/bookrental/dao/rowmapper/UserRowMapper.java
@@ -0,0 +1,29 @@
+package hu.mik.prog5.assignment.bookrental.dao.rowmapper;
+
+import hu.mik.prog5.assignment.bookrental.dao.RoleDao;
+import hu.mik.prog5.assignment.bookrental.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+@RequiredArgsConstructor
+public class UserRowMapper implements RowMapper<User> {
+
+    private final RoleDao roleDao;
+
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Long userId = rs.getLong("id");
+
+        return User.builder()
+                .id(userId)
+                .username(rs.getString("username"))
+                .password(rs.getString("password"))
+                .roles(this.roleDao.findByUserId(userId))
+                .build();
+    }
+}
