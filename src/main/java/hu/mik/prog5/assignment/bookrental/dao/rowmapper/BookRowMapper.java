2030fa65fa2944a7e5b82e911b2862978f3f29da
Tamás Nichter <nichtert99@gmail.com>
2022-12-12 10:07:00 +0100

    feat(mapping): add book row mapper

---
+++ src/main/java/hu/mik/prog5/assignment/bookrental/dao/rowmapper/BookRowMapper.java
@@ -0,0 +1,31 @@
+package hu.mik.prog5.assignment.bookrental.dao.rowmapper;
+
+import hu.mik.prog5.assignment.bookrental.entity.Book;
+import hu.mik.prog5.assignment.bookrental.entity.Genre;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class BookRowMapper implements RowMapper<Book> {
+    @Override
+    public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Book book = new Book();
+        book.setId(rs.getLong("id"));
+        book.setIsbn(rs.getString("isbn"));
+        book.setTitle(rs.getString("title"));
+        book.setAuthor(rs.getString("author"));
+        book.setPublisher(rs.getString("publisher"));
+        book.setPageNumber(rs.getInt("page_number"));
+        book.setGenre(Genre.valueOf(rs.getString("genre")));
+        book.setRentedBy(rs.getString("rented_by"));
+        if (rs.getDate("rented_until") != null) {
+            book.setRentedUntil(rs.getDate("rented_until").toLocalDate());
+        }
+        book.setIsRented(rs.getBoolean("is_rented"));
+
+        return book;
+    }
+}
