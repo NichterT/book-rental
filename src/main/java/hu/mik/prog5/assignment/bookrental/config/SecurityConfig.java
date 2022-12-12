95bf41bec5f2f1964b3ea7a9290a0af73855baf6
Tamás Nichter <nichtert99@gmail.com>
2022-12-13 00:51:30 +0100

    feat(config): add security config

---
+++ src/main/java/hu/mik/prog5/assignment/bookrental/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package hu.mik.prog5.assignment.bookrental.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] AUTH_WHITELIST = {"/css/**", "/js/**", "/images/**", "/favicon.ico**"};
+
+    @Bean
+    public SecurityFilterChain formLoginFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorize -> {
+            authorize.antMatchers(AUTH_WHITELIST).permitAll();
+            authorize.anyRequest().authenticated();
+        }).formLogin(form -> {
+            form.loginPage("/login");
+            form.defaultSuccessUrl("/", true);
+            form.permitAll();
+        }).logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
