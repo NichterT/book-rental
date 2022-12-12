ee01a1433e19e274f443da8b40a233e5f8687b34
Tamás Nichter <nichtert99@gmail.com>
2022-12-13 00:54:59 +0100

    fix(compose): update init-postgres.sql

--- docker/initdb/init-postgres.sql
+++ docker/initdb/init-postgres.sql
@@ -43,24 +43,41 @@ CREATE TABLE user_role
 );
 
 INSERT INTO users (username, password)
-VALUES ('admin', '$2a$12$rehlX.tTkJeeEC/FMHHXAOhEbY/gTGRAdlzKkzfapPgynvqDptWOS');   -- password
+VALUES ('librarian', '$2a$12$rehlX.tTkJeeEC/FMHHXAOhEbY/gTGRAdlzKkzfapPgynvqDptWOS'); -- password
 INSERT INTO users (username, password)
-VALUES ('user', '$2a$12$rehlX.tTkJeeEC/FMHHXAOhEbY/gTGRAdlzKkzfapPgynvqDptWOS');    -- password
+VALUES ('customer', '$2a$12$rehlX.tTkJeeEC/FMHHXAOhEbY/gTGRAdlzKkzfapPgynvqDptWOS'); -- password
 
 INSERT INTO role (role_name)
-VALUES ('USER');
+VALUES ('CUSTOMER');
 INSERT INTO role (role_name)
-VALUES ('ADMIN');
+VALUES ('LIBRARIAN');
 
 INSERT INTO user_role (user_id, role_id)
 SELECT *
-FROM (SELECT u.id FROM users u WHERE u.username = 'admin') a,
-     (SELECT r.id FROM role r WHERE r.role_name = 'ADMIN') b;
+FROM (SELECT u.id FROM users u WHERE u.username = 'librarian') a,
+     (SELECT r.id FROM role r WHERE r.role_name = 'LIBRARIAN') b;
 INSERT INTO user_role (user_id, role_id)
 SELECT *
-FROM (SELECT u.id FROM users u WHERE u.username = 'admin') a,
-     (SELECT r.id FROM role r WHERE r.role_name = 'USER') b;
+FROM (SELECT u.id FROM users u WHERE u.username = 'librarian') a,
+     (SELECT r.id FROM role r WHERE r.role_name = 'CUSTOMER') b;
 INSERT INTO user_role (user_id, role_id)
 SELECT *
-FROM (SELECT u.id FROM users u WHERE u.username = 'user') a,
-     (SELECT r.id FROM role r WHERE r.role_name = 'USER') b;
+FROM (SELECT u.id FROM users u WHERE u.username = 'customer') a,
+     (SELECT r.id FROM role r WHERE r.role_name = 'CUSTOMER') b;
+
+INSERT INTO book (isbn, title, author, publisher, page_number, genre, rented_by, rented_until, is_rented)
+VALUES (9789634477822, 'Churchill I.-II. - Kéz a kézben a sorssal', 'Andrew Roberts', 'ALEXANDRA KÖNYVESHÁZ KFT.', 1136,
+        'HISTORY', NULL, NULL, FALSE);
+
+INSERT INTO book (isbn, title, author, publisher, page_number, genre, rented_by, rented_until, is_rented)
+VALUES (9789633996751, 'A Court of Thorns and Roses', 'Sarah J. Maas', 'KÖNYVMOLYKÉPZŐ KIADÓ', 512,
+        'FANTASY', NULL, NULL, FALSE);
+
+INSERT INTO book (isbn, title, author, publisher, page_number, genre, rented_by, rented_until, is_rented)
+VALUES (9789634059165, 'A ragyogás', 'Stephen King', 'Európa Kiadó', 440,
+        'HORROR', NULL, NULL, FALSE);
+
+INSERT INTO book (isbn, title, author, publisher, page_number, genre, rented_by, rented_until, is_rented)
+VALUES (9789632885810, 'Állj ki magadért! - Hogyan lehetsz erős és magabiztos?', 'Gershen Kaufman - Lev Raphael',
+        'HARMAT KIADÓI ALAPITVÁNY', 184,
+        'CHILDREN', NULL, NULL, FALSE);
